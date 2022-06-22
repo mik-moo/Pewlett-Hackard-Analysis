f88beb94153d6109d26620ae593ba7b1577ff98a
mik-moo <104875218+mik-moo@users.noreply.github.com>
2022-06-22 11:35:03 -0500

    Adding queries

---
+++ queries.sql
@@ -0,0 +1,115 @@
+-- -- Creating tables for PH-EmployeeDB
+-- CREATE TABLE departments (
+--      dept_no VARCHAR(4) NOT NULL,
+--      dept_name VARCHAR(40) NOT NULL,
+--      PRIMARY KEY (dept_no),
+--      UNIQUE (dept_name)
+-- );
+
+-- CREATE TABLE employees (
+-- 	 emp_no INT NOT NULL,
+--      birth_date DATE NOT NULL,
+--      first_name VARCHAR NOT NULL,
+--      last_name VARCHAR NOT NULL,
+--      gender VARCHAR NOT NULL,
+--      hire_date DATE NOT NULL,
+--      PRIMARY KEY (emp_no)
+-- );
+
+-- CREATE TABLE dept_manager (
+-- 	dept_no VARCHAR(4) NOT NULL,
+--     emp_no INT NOT NULL,
+--     from_date DATE NOT NULL,
+--     to_date DATE NOT NULL,
+-- FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+-- FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+--     PRIMARY KEY (emp_no, dept_no)
+-- );
+-- CREATE TABLE salaries (
+--   emp_no INT NOT NULL,
+--   salary INT NOT NULL,
+--   from_date DATE NOT NULL,
+--   to_date DATE NOT NULL,
+-- FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+--   PRIMARY KEY (emp_no)
+-- );
+
+-- CREATE TABLE titles (
+--   emp_no INT NOT NULL,
+--   title VARCHAR NOT NULL,
+--   from_date DATE NOT NULL,
+--   to_date DATE NOT NULL,
+-- FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+-- );
+
+-- CREATE TABLE dept_emp (
+--     emp_no INT NOT NULL,
+-- 	dept_no VARCHAR(4) NOT NULL,
+--     from_date DATE NOT NULL,
+--     to_date DATE NOT NULL,
+-- 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+-- 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+--     PRIMARY KEY (emp_no, dept_no)
+-- );
+
+-- SELECT * FROM departments;
+
+-- SELECT * FROM employees;
+
+-- SELECT * FROM salaries;
+
+-- SELECT * FROM dept_manager;
+
+-- SELECT * FROM dept_emp;
+
+-- SELECT * FROM titles;
+
+--select employees that are about to retire.
+-- SELECT COUNT (first_name) 
+-- FROM employees
+-- WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31' ;
+
+-- --Select only employees born in 1952
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+
+-- --Select only employees born in 1953
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+
+-- --Select only employees born in 1954
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31';
+
+
+-- --Select only employees born in 1955
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31';
+
+--select employees that are about to retire who were hired between 85-88.
+-- SELECT first_name, last_name
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31') ;
+
+-- Number of employees retiring COUNT
+-- SELECT COUNT(first_name)
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
+-- --make a new table of retirement
+-- SELECT first_name, last_name
+-- INTO retirement_info
+-- FROM employees
+-- WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+-- AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * from retirement_info
