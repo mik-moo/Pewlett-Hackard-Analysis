b8d803f4c8ffd82fb73ebfcabf407c3601d61e1a
mik-moo <104875218+mik-moo@users.noreply.github.com>
2022-06-23 13:43:54 -0500

    updating to include additional queries

--- Queries/Employee_Database_Challenge.sql
+++ Queries/Employee_Database_Challenge.sql
@@ -50,3 +50,87 @@ INNER JOIN titles as t
 ON (e.emp_no = t.emp_no)
 WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
 ORDER BY e.emp_no;
+
+
+
+
+--Additional Queries
+--retrieve number of employees, titles for mentorship program
+SELECT COUNT (title), title AS "mentor title"
+INTO mentoring_titles
+FROM mentor_elig
+GROUP BY title
+ORDER BY count DESC;
+
+-- --Younger employees eligible for the manager mentorship program (none returned)
+-- SELECT DISTINCT ON (e.emp_no)
+-- 	e.emp_no,
+-- 	e.first_name,
+-- 	e.last_name,
+-- 	e.birth_date,
+-- 	de.from_date,
+-- 	de.to_date,
+-- 	t.title
+-- --INTO younger_mentor_elig
+-- FROM employees as e
+-- INNER JOIN dept_emp as de
+-- ON (e.emp_no = de.emp_no)
+-- INNER JOIN titles as t
+-- ON (e.emp_no = t.emp_no)
+-- WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1966-01-01' AND '1966-12-31') AND (t.title = 'Manager')
+-- ORDER BY e.emp_no;
+
+--Older employees eligible for the manager mentorship program
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mgmt_mentor_elig_64
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1964-01-01' AND '1964-12-31') AND (t.title = 'Manager')
+ORDER BY e.emp_no;
+
+-- Increase age/birth date range for mentor program from '56-'65
+SELECT DISTINCT ON (e.emp_no)
+	e.emp_no,
+	e.first_name,
+	e.last_name,
+	e.birth_date,
+	de.from_date,
+	de.to_date,
+	t.title
+INTO mgmt_mentor_elig_56_65
+FROM employees as e
+INNER JOIN dept_emp as de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles as t
+ON (e.emp_no = t.emp_no)
+WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1956-01-01' AND '1964-12-31') AND (t.title = 'Manager')
+ORDER BY e.emp_no;
+
+
+-- --employees eligible for the mentorship program 66-70--none identified
+-- SELECT DISTINCT ON (e.emp_no)
+-- 	e.emp_no,
+-- 	e.first_name,
+-- 	e.last_name,
+-- 	e.birth_date,
+-- 	de.from_date,
+-- 	de.to_date,
+-- 	t.title
+-- --INTO mentor_elig
+-- FROM employees as e
+-- INNER JOIN dept_emp as de
+-- ON (e.emp_no = de.emp_no)
+-- INNER JOIN titles as t
+-- ON (e.emp_no = t.emp_no)
+-- WHERE (de.to_date = '9999-01-01') AND (e.birth_date BETWEEN '1966-01-01' AND '1970-12-31') AND (t.title = 'Manager')
+-- ORDER BY e.emp_no;
